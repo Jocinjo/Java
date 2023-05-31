0b5b89ddf9d5d58cf33dab51fc293f7a64a780e2
Jocinjo <josie.knipping@gmail.com>
2023-05-31 09:37:36 +0200

    trying to commit from visual code

--- Main.java
+++ Main.java
@@ -1,16 +1,76 @@
 // dag 2 3 van de 3
 import java.util.Scanner;
+// import java.util.Random;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Wat is je naam? ");
+
+// public class Main {
+//     public static void main(String[] args) {
+        // System.out.println("Wat is je naam? ");
+        // Scanner input = new Scanner(System.in);
+        // String naam = input.nextLine();
+        // System.out.println("Bread is ready.");
+        // System.out.println("Who's the bread for?");
+        // System.out.println(naam);
+        // System.out.println("Noted: " + naam + " got bread.");
+        // input.close();
+
+        
+    // Random random = new Random();
+    // boolean lichtAan = random.nextBoolean();
+    // lamp lamp1 = new lamp ();
+
+    // if (lamp1.lichtAan= true) {
+    //     System.out.println("het licht is aan");
+    // }else {
+    //     System.out.println("het is niet aan.");
+    // }
+    // }
+
+
+
+    public class Main {
+        public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String naam = input.nextLine();
-        System.out.println("Bread is ready.");
-        System.out.println("Who's the bread for?");
-        System.out.println(naam);
-        System.out.println("Noted: " + naam + " got bread.");
+        System.out.println("welke dag van de week is het? ");
+        String weekdag = input.next().toLowerCase();
+        int dagnummer;
         input.close();
-    }
-}
+    
+        if (weekdag.equals("maandag")) {
+            dagnummer = 0;
+            System.out.println("het is maandag, dag: " + dagnummer);
+        }
+        else if (weekdag.equals("dinsdag")) {
+            dagnummer = 1;
+            System.out.println("het is dinsdag, dag: " + dagnummer);
+        }
+        else if (weekdag.equals("woensdag")) {
+            dagnummer = 2;
+            System.out.println("het is woensdag, dag: " + dagnummer);
+        }
+        else if (weekdag.equals("donderdag")) {
+            dagnummer = 3;
+            System.out.println("het is donderdag, dag: " + dagnummer);
+        }
+        else if (weekdag.equals("vrijdag")) {
+            dagnummer = 4;
+            System.out.println("het is vrijdag, dag: " + dagnummer);
+        }
+        else if (weekdag.equals("zaterdag")) {
+            dagnummer = 5;
+            System.out.println("het is zaterdag, dag: " + dagnummer);
+        }
+        else if (weekdag.equals("zondag")) {
+            dagnummer = 6;
+            System.out.println("het is zondag, dag: " + dagnummer);
+        }
+        else {
+                    System.out.println("dit is geen geldige dag!");
+        }
+        }
+    
+    
+    
 
+
+}
---
+++ lamp.java
@@ -0,0 +1,14 @@
+// import java.util.Random;
+
+// public class lamp {
+//     boolean lichtAan = true;
+
+//     public lamp() {
+//         boolean lichtAan = true;
+//         Random random = new Random();
+//         this.lichtAan = random.nextBoolean();
+    //     if (lichtAan == true) {
+    //         System.out.println("het licht is aan");
+    // } else {System.out.println("Licht is uit.");}
+//     }
+// }
